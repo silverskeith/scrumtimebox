090c6ae69481c8e6dbb0534628edc95dc9d2cab7
silverskeith <iti.raymond@yahoo.com>
2020-03-25 17:57:38 +0800

    Updates

---
+++ precache-manifest.ccd007f852e788a12276f6d8eb65b694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85b6ec0456b43b606cb586867a5f72b",
+    "url": "/scrumtimebox/index.html"
+  },
+  {
+    "revision": "201e33286c1d8c6e5747",
+    "url": "/scrumtimebox/static/css/main.d56605c8.chunk.css"
+  },
+  {
+    "revision": "37190fcac05f4eb3aea1",
+    "url": "/scrumtimebox/static/js/2.b1059f45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scrumtimebox/static/js/2.b1059f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201e33286c1d8c6e5747",
+    "url": "/scrumtimebox/static/js/main.2fa0cf1c.chunk.js"
+  },
+  {
+    "revision": "64d7cc34ebae5ef6d23b",
+    "url": "/scrumtimebox/static/js/runtime-main.a68d2754.js"
+  }
+]);
