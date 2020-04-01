3496281e47e1407c03c9882f94cdc6e48714801c
silverskeith <iti.raymond@yahoo.com>
2020-04-01 21:32:30 +0800

    Split minute and second inputs

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,18 +4,20 @@ import Clock from "./Clock";
 import Button from "./Button";
 import Input from "./Input";
 
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.inputRef = React.createRef();
     this.state = {
+      seconds: 0,
+      minutes: 0,
       count: 0,
       running: false
     };
   }
 
   componentDidUpdate(prevProps, prevState) {
-    this.refs[this.state.count].refs.seconds.value = this.state.count;
-
     if (this.state.running !== prevState.running) {
       switch (this.state.running) {
         case true:
@@ -26,31 +28,46 @@ export default class App extends React.Component {
     }
   }
 
+  putTimeValue(second) {
+    const secondVal = (second > 60) ? second - 60 : second;
+    const minuteVal = Math.floor(second / 60);
+
+    this.setState({ minutes: minuteVal, seconds: (secondVal - 1), count: (parseInt(Math.floor(minuteVal * 60)) + parseInt(secondVal))});
+
+    this.inputRef.current.refs.seconds.focus();
+    this.inputRef.current.refs.seconds.value = secondVal - 1;
+    
+    this.inputRef.current.refs.minutes.focus();
+    this.inputRef.current.refs.minutes.value = minuteVal;
+  }
+
   handleStart() {
     this.timer = setInterval(() => {
       const newCount = this.state.count - 1;
-      this.setState({ count: newCount >= 0 ? newCount : 0 });
+      this.setState({ count: newCount >= 0 ? newCount : 0, minutes: (Math.floor(newCount / 60)) });
     }, 1000);
   }
 
   handleStop() {
-    this.refs[this.state.count].refs.seconds.value = this.state.count;
     if (this.timer) {
       clearInterval(this.timer);
+      this.putTimeValue(this.state.count);
       this.setState({ running: false });
     }
   }
 
   handleReset() {
+    clearInterval(this.timer);
     this.setState({ count: 0 });
   }
 
   handleCountdown(seconds) {
-    this.setState({ count: seconds, running: true });
+    this.putTimeValue(seconds);
+    this.setState({ count: seconds - 1, running: true });
   }
 
   handlePreset(seconds) {
-    this.refs[this.state.count].refs.seconds.value = seconds;
+    this.putTimeValue(seconds);
     this.setState({ count: seconds, running: true });
   }
 
@@ -72,7 +89,7 @@ export default class App extends React.Component {
           onClickHandler={this.handlePreset.bind(this, 300)}
         />
         <Input
-          ref={this.state.count}
+          ref={this.inputRef}
           onSetCountdown={this.handleCountdown.bind(this)}
         />
         <Button label="Stop" onClickHandler={this.handleStop.bind(this)} />
