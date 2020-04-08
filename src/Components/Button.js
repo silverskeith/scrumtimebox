346b7530ccbe0c50dca52c96e3595698068fe84e
silverskeith <iti.raymond@yahoo.com>
2020-04-08 16:03:57 +0800

    Added design effects and fixed bug in input

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Button extends React.Component {
     render() {
         return (
-            <button onClick={this.props.onClickHandler}>{this.props.label}</button>
+            <button className='waves-effect waves-light' onClick={this.props.onClickHandler}>{this.props.label}</button>
         )
     }
 }
